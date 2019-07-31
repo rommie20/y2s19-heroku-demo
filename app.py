92931c1a4f910270eecbdffabca346db5473ca41
myname21-meet <myname21@meet.mit.edu>
2019-07-31 05:04:52 -0400

    uploaded web

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home_page():
+	list1 =["pasta", "sushi", "fish"]
+    return render_template("index.html", food=list1)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+   app.run(debug = True)
